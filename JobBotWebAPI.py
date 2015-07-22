b579429ff58fe912d93d2ef0c23746a743e9f981
John OBrien <jweob@cantab.net>
2015-07-21 20:59:08 -0400

    Ubuntu stuff

--- JobBotWebAPI.py
+++ JobBotWebAPI.py
@@ -10,6 +10,205 @@ import nxt, thread, time
 #Import colorama
 from colorama import init, Fore, Back, Style
 
+class MyBaseMotor(object):
+    """Base class for motors"""
+    debug = 0
+    def _debug_out(self, message):
+        if self.debug:
+            print message
+
+    def turn(self, power, tacho_units, brake=True, timeout=1, emulate=True):
+        """Use this to turn a motor. The motor will not stop until it turns the
+        desired distance. Accuracy is much better over a USB connection than
+        with bluetooth...
+        power is a value between -127 and 128 (an absolute value greater than
+                 64 is recommended)
+        tacho_units is the number of degrees to turn the motor. values smaller
+                 than 50 are not recommended and may have strange results.
+        brake is whether or not to hold the motor after the function exits
+                 (either by reaching the distance or throwing an exception).
+        timeout is the number of seconds after which a BlockedException is
+                 raised if the motor doesn't turn
+        emulate is a boolean value. If set to False, the motor is aware of the
+                 tacho limit. If True, a run() function equivalent is used.
+                 Warning: motors remember their positions and not using emulate
+                 may lead to strange behavior, especially with synced motors
+        """
+
+        print 'Working!!!'
+        tacho_limit = tacho_units
+
+        if tacho_limit < 0:
+            raise ValueError, "tacho_units must be greater than 0!"
+        #TODO Calibrate the new values for ip socket latency.
+        if self.method == 'bluetooth':
+            threshold = 70
+        elif self.method == 'usb':
+            threshold = 5
+        elif self.method == 'ipbluetooth':
+            threshold = 80
+        elif self.method == 'ipusb':
+            threshold = 15
+        else:
+            threshold = 30 #compromise
+
+        tacho = self.get_tacho()
+        state = self._get_new_state()
+
+        # Update modifiers even if they aren't used, might have been changed
+        state.power = power
+        if not emulate:
+            state.tacho_limit = tacho_limit
+
+        self._debug_out('Updating motor information...')
+        self._set_state(state)
+
+        direction = 1 if power > 0 else -1
+        self._debug_out('tachocount: ' + str(tacho))
+        current_time = time.time()
+        tacho_target = tacho.get_target(tacho_limit, direction)
+
+        blocked = False
+        try:
+            while True:
+                time.sleep(self._eta(tacho, tacho_target, power) / 2)
+
+                if not blocked: # if still blocked, don't reset the counter
+                    last_tacho = tacho
+                    last_time = current_time
+
+                tacho = self.get_tacho()
+                current_time = time.time()
+                blocked = self._is_blocked(tacho, last_tacho, direction)
+                if blocked:
+                    self._debug_out(('not advancing', last_tacho, tacho))
+                    # the motor can be up to 80+ degrees in either direction from target when using bluetooth
+                    if current_time - last_time > timeout:
+                        if tacho.is_near(tacho_target, threshold):
+                            break
+                        else:
+                            raise BlockedException("Blocked!")
+                else:
+                    self._debug_out(('advancing', last_tacho, tacho))
+                if tacho.is_near(tacho_target, threshold) or tacho.is_greater(tacho_target, direction):
+                    break
+        finally:
+            if brake:
+                self.brake()
+            else:
+                self.idle()
+
+class MyMotor(MyBaseMotor):
+    def __init__(self, brick, port):
+        self.brick = brick
+        self.port = port
+        self._read_state()
+        self.sync = 0
+        self.turn_ratio = 0
+        try:
+            self.method = brick.sock.type
+        except:
+            print "Warning: Socket did not report a type!"
+            print "Please report this problem to the developers!"
+            print "For now, turn() accuracy will not be optimal."
+            print "Continuing happily..."
+            self.method = None
+
+    def _set_state(self, state):
+        self._debug_out('Setting brick output state...')
+        list_state = [self.port] + state.to_list()
+        self.brick.set_output_state(*list_state)
+        self._debug_out(state)
+        self._state = state
+        self._debug_out('State set.')
+
+    def _read_state(self):
+        self._debug_out('Getting brick output state...')
+        values = self.brick.get_output_state(self.port)
+        self._debug_out('State got.')
+        self._state, tacho = get_tacho_and_state(values)
+        return self._state, tacho
+
+    #def get_tacho_and_state here would allow tacho manipulation
+
+    def _get_state(self):
+        """Returns a copy of the current motor state for manipulation."""
+        return OutputState(self._state.to_list())
+
+    def _get_new_state(self):
+        state = self._get_state()
+        if self.sync:
+            state.mode = MODE_MOTOR_ON | MODE_REGULATED
+            state.regulation = REGULATION_MOTOR_SYNC
+            state.turn_ratio = self.turn_ratio
+        else:
+            state.mode = MODE_MOTOR_ON | MODE_REGULATED
+            state.regulation = REGULATION_MOTOR_SPEED
+        state.run_state = RUN_STATE_RUNNING
+        state.tacho_limit = LIMIT_RUN_FOREVER
+        return state
+
+    def get_tacho(self):
+        return self._read_state()[1]
+
+    def reset_position(self, relative):
+        """Resets the counters. Relative can be True or False"""
+        self.brick.reset_motor_position(self.port, relative)
+
+    def run(self, power=100, regulated=False):
+        '''Tells the motor to run continuously. If regulated is True, then the
+        synchronization starts working.
+        '''
+        state = self._get_new_state()
+        state.power = power
+        if not regulated:
+            state.mode = MODE_MOTOR_ON
+        self._set_state(state)
+
+    def brake(self):
+        """Holds the motor in place"""
+        state = self._get_new_state()
+        state.power = 0
+        state.mode = MODE_MOTOR_ON | MODE_BRAKE | MODE_REGULATED
+        self._set_state(state)
+
+    def idle(self):
+        '''Tells the motor to stop whatever it's doing. It also desyncs it'''
+        state = self._get_new_state()
+        state.power = 0
+        state.mode = MODE_IDLE
+        state.regulation = REGULATION_IDLE
+        state.run_state = RUN_STATE_IDLE
+        self._set_state(state)
+
+    def weak_turn(self, power, tacho_units):
+        """Tries to turn a motor for the specified distance. This function
+        returns immediately, and it's not guaranteed that the motor turns that
+        distance. This is an interface to use tacho_limit without
+        REGULATION_MODE_SPEED
+        """
+        tacho_limit = tacho_units
+        tacho = self.get_tacho()
+        state = self._get_new_state()
+
+        # Update modifiers even if they aren't used, might have been changed
+        state.mode = MODE_MOTOR_ON
+        state.regulation = REGULATION_IDLE
+        state.power = power
+        state.tacho_limit = tacho_limit
+
+        self._debug_out('Updating motor information...')
+        self._set_state(state)
+
+    def _eta(self, current, target, power):
+        """Returns time in seconds. Do not trust it too much"""
+        tacho = abs(current.tacho_count - target.tacho_count)
+        return (float(tacho) / abs(power)) / 5
+
+    def _is_blocked(self, tacho, last_tacho, direction):
+        """Returns if any of the engines is blocked"""
+        return direction * (last_tacho.tacho_count - tacho.tacho_count) >= 0
+
 brick = True
 
 try:
@@ -42,9 +241,9 @@ import os, os.path
 MEDIA_DIR = os.path.join(os.path.abspath("."), "Media")
 
 if brick:
-    mx = nxt.Motor(b, nxt.PORT_A)
-    my = nxt.Motor(b, nxt.PORT_B)
-    mz = nxt.Motor(b, nxt.PORT_C)
+    mx = MyMotor(b, nxt.PORT_A)
+    my = MyMotor(b, nxt.PORT_B)
+    mz = MyMotor(b, nxt.PORT_C)
     motors = [mx, my, mz]
 
 def turnmotor(m, power, degrees):
